d0d974237450106f1a7df30923e28a85f50361c9
Dananjay996 <sreedananjay456@gmail.com>
2023-04-28 16:23:46 +0530

    Initialized project

---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+const data = [
+    { id: 1, name: "Node 1", type: "start", children: [2, 3] },
+    { id: 2, name: "Node 2", type: "process", children: [4] },
+    { id: 3, name: "Node 3", type: "decision", children: [5, 6] },
+    { id: 4, name: "Node 4", type: "process", children: [] },
+    { id: 5, name: "Node 5", type: "process", children: [] },
+    { id: 6, name: "Node 6", type: "process", children: [] },
+  ];
